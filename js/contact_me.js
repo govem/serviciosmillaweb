dd531bda1510683d74c390723a4620f35914678d
Gonzalo <gvega@wit-labs.com>
2017-09-22 18:18:33 -0300

    cambio en url de form, para apuntar a heroku

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "http://www.serviciosmilla.cl/mail/contact_me.php",
+        url: "https://serviciosmillaweb.herokuapp.com/mail/contact_me.php",
         type: "POST",
         data: {
           name: name,
