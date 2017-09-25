4ef0003c595dab3d1172356efc03634d73516993
Gonzalo Vega <gonzalo.vega@gmail.com>
2017-09-25 16:55:51 -0300

    se agrega sendgrid

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,4 +1,5 @@
 <?php
+require 'vendor/autoload.php';
 
 switch ($_SERVER['HTTP_ORIGIN']) {
     case 'http://serviciosmilla.cl': case 'https://serviciosmilla.cl':
@@ -31,6 +32,21 @@ $email_subject = "Contacto desde sitio web:  $name";
 $email_body = "Tienes un nuevo mensaje desde el sitio web.\n\n"."Aquí están los detalles:\n\nNombre: $name\n\nEmail: $email_address\n\nTeléfono: $phone\n\nMensaje:\n$message";
 $headers = "From: noreply@serviciosmilla.cl\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
-mail($to,$email_subject,$email_body,$headers);
-return true;         
+//mail($to,$email_subject,$email_body,$headers);
+//return true;         
+
+$from = new SendGrid\Email(null, $email_address);
+$subject = $email_subject;
+$to = new SendGrid\Email(null, $to);
+$content = new SendGrid\Content("text/plain", $email_body);
+$mail = new SendGrid\Mail($from, $subject, $to, $content);
+
+$apiKey = getenv('SENDGRID_API_KEY');
+$sg = new \SendGrid($apiKey);
+
+$response = $sg->client->mail()->send()->post($mail);
+echo $response->statusCode();
+echo $response->headers();
+echo $response->body();
+return true;
 ?>
