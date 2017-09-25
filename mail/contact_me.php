e1e07df054862c16477228a7d3359083cf56d886
Gonzalo Vega <gonzalo.vega@gmail.com>
2017-09-25 14:42:18 -0300

    cors

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,4 +1,14 @@
 <?php
+
+switch ($_SERVER['HTTP_ORIGIN']) {
+    case 'http://www.serviciosmilla.cl': case 'https://www.serviciosmilla.cl':
+    header('Access-Control-Allow-Origin: '.$_SERVER['HTTP_ORIGIN']);
+    header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+    header('Access-Control-Max-Age: 1000');
+    header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+    break;
+}
+
 // Check for empty fields
 if(empty($_POST['name'])      ||
    empty($_POST['email'])     ||
