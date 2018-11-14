6b23c67152d1e8f883ba22cda81a1958731566e8
Gonzalo <gvega@wit-labs.com>
2018-11-13 22:53:56 -0300

    content type header

--- mail/pedir_hora.php
+++ mail/pedir_hora.php
@@ -7,6 +7,7 @@ switch ($_SERVER['HTTP_ORIGIN']) {
     header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
     header('Access-Control-Max-Age: 1000');
     header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+    header('Content-type:application/json');
     break;
 }
    
